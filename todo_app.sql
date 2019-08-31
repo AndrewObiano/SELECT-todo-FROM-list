0818ba980f323b183d869b52dc1c4fc0c1fb5385
Andrew Obiano <andrew.obiano@gmail.com>
2019-08-30 23:21:18 -1000

    Finished

---
+++ todo_app.sql
@@ -0,0 +1,70 @@
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR (255),
+    description text,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    completed boolean
+);
+
+ALTER TABLE tasks
+DROP completed;
+
+ALTER TABLE tasks
+ADD completed_at timestamp
+DEFAULT NULL;
+
+ALTER TABLE tasks
+ALTER updated_at SET NOT NULL;
+
+ALTER TABLE tasks
+ALTER updated_at SET DEFAULT now();
+
+INSERT INTO tasks
+VALUES (default, 'Study SQL', 'Complete this exercise', now(), now(), NULL);
+
+INSERT INTO tasks (title, description)
+VALUES ('Study PostgresQL', 'Read all the documentation');
+
+SELECT title
+FROM tasks
+WHERE completed_at is NULL;
+
+UPDATE tasks
+SET completed_at = now()
+WHERE title = 'Study SQL';
+
+SELECT title, description
+FROM tasks
+WHERE completed_at is NULL;
+
+SELECT *
+FROM tasks
+ORDER BY created_at DESC;
+
+INSERT INTO tasks (title, description)
+VALUES ('mistake 1', 'a test entry');
+
+INSERT INTO tasks (title, description)
+VALUES ('mistake 2', 'another test entry');
+
+INSERT INTO tasks (title, description)
+VALUES ('third mistake', 'another test entry');
+
+SELECT title
+FROM tasks
+WHERE title LIKE '%mistake%';
+
+DELETE
+FROM tasks
+WHERE title = 'mistake 1';
+
+SELECT title, description
+FROM tasks
+WHERE title LIKE '%mistake%';
+
+SELECT *
+FROM tasks
+ORDER BY title ASC;
